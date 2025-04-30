3d18a2ce018752a00271bc2b23e7d0d6cf9f2941
raphaelcseb <raphaelcseb@al.insper.edu.br>
2025-04-30 14:47:21 -0300

    exercício 2 - conceito D

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,10 @@ def rolar_dados (n):
         dados.append(dado)
     return dados
 
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+   
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+   
+    del(dados_rolados[dado_para_guardar])
+    
+    return [dados_rolados,dados_no_estoque]
